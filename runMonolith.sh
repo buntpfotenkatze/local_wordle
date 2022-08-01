6cb6664158d0f2a0227b62153b89904b07ba7da4
pinkpawkitty <git@lara.uber.space>
2022-08-01 23:53:52 +0200

    Fix save icon size

---
+++ runMonolith.sh
@@ -0,0 +1,2 @@
+monolith -o wordle.html src/wordle.html
+npx prettier --write wordle.html
--- updateWordleSrc.sh
+++ updateWordleSrc.sh
@@ -1,2 +1,1 @@
 wget -mpk https://www.nytimes.com/games/wordle/index.html
-monolith -o wordle.html src/wordle.html
